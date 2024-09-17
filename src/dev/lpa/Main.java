be548c5be99be2896ad6a82852aa592b51da7d52
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-17 20:38:24 +0200

    add new Thread(myRunnable) using lambda

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -16,6 +18,31 @@ public class Main {
 
     CustomThread customThread = new CustomThread();
     customThread.start();
+
+    Runnable myRunnable = () -> {
+
+      for (int i = 0; i < 8; i++) {
+
+        System.out.print(" 2 ");
+        try {
+          TimeUnit.MILLISECONDS.sleep(250);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+
+    Thread myThread = new Thread(myRunnable);
+    myThread.start();
+
+    for (int i = 0; i < 3; i++) {
+      System.out.print(" 0 ");
+      try {
+        TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+    }
   }
 
   public static void printThreadState(Thread thread) {
